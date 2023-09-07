95447538a5ba94a45d170fb2062a6f7c7c7708b2
TomerPopik <tomer.p@pepperi.com>
2023-09-07 23:43:38 +0300

    - Finish the CPI side

--- cpi-side/services/get-values.service.ts
+++ cpi-side/services/get-values.service.ts
@@ -1,73 +1,43 @@
 import { IClient, IContext } from '@pepperi-addons/cpi-node/build/cpi-side/events';
-import { FieldType, parse, toApiQueryString, filter, JSONFilter, JSONRegularFilter } from '@pepperi-addons/pepperi-filters'
+import { FieldType, parse, toApiQueryString, filter, JSONFilter, JSONBaseFilter } from '@pepperi-addons/pepperi-filters'
 import { GetValueOption, GetValuesConifuration, GetValuesMappedConifuration } from 'shared';
 
 class GetValuesCpiService {
 
-    private getParamValue(param: any, context: any): any {
-        let paramValue = param?.Value || '';
-
-        // If the param source is dynamic, we will get the param value from the context.
-        if (param?.FlowParamSource === 'Dynamic') {
-            paramValue = context[paramValue] || '';
+    private setDynamicValuesInFilter(jsonFilter: JSONFilter, context: any) {
+        if (jsonFilter.Operation === 'AND' || jsonFilter.Operation === 'OR') {
+            this.setDynamicValuesInFilter(jsonFilter.LeftNode, context);
+            this.setDynamicValuesInFilter(jsonFilter.RightNode, context);
+        } else {
+            // If the value type is dynamic, we will override the value from the context.
+            if (jsonFilter['ValueType'] === 'Dynamic' && (jsonFilter as JSONBaseFilter).Values?.length > 0) {
+                const paramValue = (jsonFilter as JSONBaseFilter).Values[0];
+                (jsonFilter as JSONBaseFilter).Values[0] = context[paramValue] || '';
+            }
         }
-
-        return paramValue;
     }
 
-    private getValue(object: any, apiName: string): any {
-        if (!apiName) {
-            return undefined;
-        }
-
-        if (typeof object !== 'object') {
-            return undefined;
-        }
-
-        // support regular APINames & dot anotation
-        if (apiName in object) {
-            return object[apiName];
-        }
-
-        // support nested object & arrays
-        apiName = apiName.replace(/\[(\w+)\]/g, '.$1'); // convert indexes to properties
-        const arr = apiName.split('.');
-        return this.getValue(object[arr[0]], arr.slice(1).join('.'));
-    }
-
-    private async getValuesFromMappedData(mappedData: GetValuesMappedConifuration | undefined): Promise<GetValueOption[]> {
+    private async getValuesFromMappedData(mappedData: GetValuesMappedConifuration | undefined, context: any): Promise<GetValueOption[]> {
         let options: GetValueOption[] = [];
 
         if (mappedData && mappedData.Resource && mappedData.ResourceOptionKeyField && mappedData.ResourceOptionTitleField) {
-            let qsFilter = '';
 
-            // If there is query need to build the filter
+            // If there is query need to build the filter, set the flow params values in the filter if needed.
             if (mappedData.ResourceQuery) {
-                // TODO: parse the filter and set the flow params values if needed.
-                const resourceQuery: JSONFilter = mappedData.ResourceQuery;
-
-                qsFilter = toApiQueryString(resourceQuery) || '';
-                // const a = toNgxFilter(resourceQuery);
+                this.setDynamicValuesInFilter(mappedData.ResourceQuery, context);
             }
 
-            const resource = await pepperi.resources.resource(mappedData.Resource).search({
-                // Where: qsFilter,
-                // Fields: [mappedData.ResourceOptionKeyField, mappedData.ResourceOptionTitleField],
-            });
-            debugger;
-                
-            const res = filter(resource.Objects, mappedData.ResourceQuery, this.getValue);
+            const resource = await pepperi.resources.resource(mappedData.Resource).search({});
+            const res = filter(resource.Objects, mappedData.ResourceQuery);
 
             if (res) {
-
-                debugger;
+                options = res.map(obj => {
+                    return {
+                        Key: mappedData?.ResourceOptionKeyField ? obj[mappedData.ResourceOptionKeyField] : '',
+                        Title: mappedData?.ResourceOptionTitleField ? obj[mappedData.ResourceOptionTitleField] : ''
+                    }
+                }) || [];
             }
-            options = resource?.Objects.map(obj => {
-                return {
-                    Key: mappedData?.ResourceOptionKeyField ? obj[mappedData.ResourceOptionKeyField] : '',
-                    Title: mappedData?.ResourceOptionTitleField ? obj[mappedData.ResourceOptionTitleField] : ''
-                }
-            }) || [];
         }
 
         return options;
@@ -80,13 +50,12 @@ class GetValuesCpiService {
             const client: IClient | undefined = context?.client || undefined;
 
             if (client) {
-                // debugger;
                 const data: GetValuesConifuration = body || {};
 
                 if (data.ValuesType === 'manual') {
                     values.push(...data.ManualOptions.map(option => { return { Key: option.key, Title: option.value } }));
                 } else { // if (data.ValuesType === 'mapped')
-                    values.push(...await this.getValuesFromMappedData(data.MappedData));
+                    values.push(...await this.getValuesFromMappedData(data.MappedData, context));
                 }
             }
         }
