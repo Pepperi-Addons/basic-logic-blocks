1156a77890f94af6aed39009a40f6d17f3bc2414
Dor Shpigel <dor.s@pepperi.com>
2024-06-17 10:31:01 +0300

    version bump

--- cpi-side/services/extract-value.service.ts
+++ cpi-side/services/extract-value.service.ts
@@ -11,7 +11,7 @@ class ExtractValueCpiService extends BaseCpiService {
       return {
         Value: { Value: getValue, Type: this.getType(getValue) },
       };
-    } catch(e) {
+    } catch (e) {
       console.log((e as Error).message);
       return {};
     }
